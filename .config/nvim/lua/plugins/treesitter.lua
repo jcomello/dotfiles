65a77f78b7e8958b6877aaf9938aa1455d80347d
João Mello <joao.mello@smartfit.com>
2024-04-24 13:11:18 -0300

    Fix keymaps for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -9,8 +9,6 @@ return {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
-      },
-      {
         incremental_selection = {
           enable = true,
           keymaps = {
