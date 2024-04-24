3ddf761974f05591b7379c14e9f1e13937efbe83
João Mello <joao.mello@smartfit.com>
2024-04-24 12:35:37 -0300

    Improve configs

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,21 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "ruby", "rust" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+      },
+      {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<Leader>ss",
+            node_incremental = "<Leader>si",
+            scope_incremental = "<Leader>sc",
+            node_decremental = "<Leader>sd",
+          },
+        },
       })
     end
   }
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,6 +1,8 @@
 vim.opt.number  = true
 vim.opt.relativenumber = true
 
+vim.opt.swapfile = false
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
