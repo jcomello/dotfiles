bd29246b7575965754ca8207334e14ba8ef9a3cc
João Mello <joao.mello@smartfit.com>
2024-05-28 13:31:18 -0300

    Add a dashboard to nvim

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,18 @@
+return {
+    'MeanderingProgrammer/dashboard.nvim',
+    event = 'VimEnter',
+    dependencies = {
+        { 'MaximilianLloyd/ascii.nvim', dependencies = { 'MunifTanjim/nui.nvim' } },
+    },
+    config = function()
+        require('dashboard').setup({
+            header = require('ascii').art.text.neovim.sharp,
+            directories = {
+                '~/dotfiles',
+                '~/dotfiles/.config/nvim',
+                '~/development/smartfit/Smart-System',
+                '~/development/smartfit/themis',
+            },
+        })
+    end,
+}
