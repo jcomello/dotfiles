728ea2a86bff45233e104a201fa5605434c18808
João Mello <joaocarreira88@gmail.com>
2024-07-08 18:49:30 -0300

    Improve config for the new pc

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     lazy = false,
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--       vim.cmd.colorscheme "catppuccin-mocha"
+--     end
+--   }
+-- }
+return {
+  "tiagovla/tokyodark.nvim",
+  opts = {
+      -- custom options here
+  },
+  config = function(_, opts)
+    require("tokyodark").setup(opts) -- calling setup is optional
+    vim.cmd [[colorscheme tokyodark]]
+  end,
+}
