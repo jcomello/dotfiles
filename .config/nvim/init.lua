adb889a648faf8f07c58d230aa6246a13bc9c826
João Mello <joaocarreira88@gmail.com>
2024-10-10 13:58:21 -0300

    Add hightlight for yanked text

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,14 @@
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
