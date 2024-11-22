28cf72ee9d59eb70128a2f11b98e8a4854242e48
João Mello <joaocarreira88@gmail.com>
2024-11-22 01:14:31 -0300

    Fix nvim setup error

--- setup.sh
+++ setup.sh
@@ -48,7 +48,6 @@ echo "---------------------------"
 echo "Installing nvim..."
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 echo "---------------------------"
 
