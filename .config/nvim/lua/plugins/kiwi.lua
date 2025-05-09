0ab43ebe3bcf9729772c18e5a5694cc03cc95098
João Mello <joao.mello@suse.com>
2025-05-09 13:29:45 -0300

    Use original repository again

--- .config/nvim/lua/plugins/kiwi.lua
+++ .config/nvim/lua/plugins/kiwi.lua
@@ -1,5 +1,5 @@
 return {
-	"jcomello/kiwi.nvim",
+	"serenevoid/kiwi.nvim",
 	keys = {
 		{ "<leader>ww", ':lua require("kiwi").open_wiki_index()<cr>', desc = "Open Wiki index" },
 		{ "T", ':lua require("kiwi").todo.toggle()<cr>', desc = "Toggle Markdown Task" },
