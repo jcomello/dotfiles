206e0766d6571558cc7dae1a483fcc59f46f0b8f
João Mello <joao.mello@suse.com>
2025-05-09 15:56:31 -0300

    Format all config files

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,19 +2,19 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.warn_about_missing_glyphs = false
-config.font = wezterm.font_with_fallback {
-  'Jetbrains Mono Nerd Font',
-  'Fira Code'
-}
+config.font = wezterm.font_with_fallback({
+	"Jetbrains Mono Nerd Font",
+	"Fira Code",
+})
 config.font_size = 15
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 0,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 0,
 }
 return config
