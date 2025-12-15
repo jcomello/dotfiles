30dfc3a757b494c40355cb68cc56221605820537
João Mello <joao.mello@suse.com>
2025-12-15 15:25:54 -0300

    Set colorscheme to tokyo night

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-  "tiagovla/tokyodark.nvim",
-  opts = {
-    -- custom options here
-  },
-  config = function(_, opts)
-    require("tokyodark").setup(opts) -- calling setup is optional
-    vim.cmd([[colorscheme tokyodark]])
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd([[colorscheme tokyonight]])
   end,
 }
