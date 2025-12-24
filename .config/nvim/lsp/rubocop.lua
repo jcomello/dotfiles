9ada2c7b8372c2745557bb4842638044a95e186c
João Mello <joao.mello@suse.com>
2025-12-24 10:01:45 -0300

    Remove lsp files that were not being used

--- .config/nvim/lsp/rubocop.lua
+++
@@ -1,8 +0,0 @@
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-return {
-  cmd = { "rubocop", "--lsp" },
-  filetypes = { "ruby", "rb" },
-  root_markers = { "Gemfile", ".git" },
-  capabilities = capabilities,
-}
